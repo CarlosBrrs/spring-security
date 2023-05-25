9a037dc8a922b164411c8a57a705168404e159a0
Carlos Barrios <carlosbarrios148@gmail.com>
2023-05-25 01:50:09 -0500

    Create AuthenticationService and call it from controller

--- src/main/java/com/carlosbrrs/security/auth/AuthenticationController.java
+++ src/main/java/com/carlosbrrs/security/auth/AuthenticationController.java
@@ -12,13 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final AuthenticationService authenticationService;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
-
+        return ResponseEntity.ok(authenticationService.register(registerRequest));
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest authenticationRequest) {
-
+        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
     }
 }
---
+++ src/main/java/com/carlosbrrs/security/auth/AuthenticationService.java
@@ -0,0 +1,45 @@
+package com.carlosbrrs.security.auth;
+
+import com.carlosbrrs.security.config.JwtService;
+import com.carlosbrrs.security.entity.user.Role;
+import com.carlosbrrs.security.entity.user.User;
+import com.carlosbrrs.security.entity.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest registerRequest) {
+        User user = User.builder()
+                .firstname(registerRequest.getFirstname())
+                .lastname(registerRequest.getLastname())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword()));
+        User user = userRepository.findByEmail(authenticationRequest.getEmail()).orElseThrow();
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
